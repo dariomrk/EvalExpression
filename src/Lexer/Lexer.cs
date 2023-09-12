fe0fe885311ba609a7fd97800e58543effdfaa18
dariomrk <dariomrkonjic1@gmail.com>
2023-09-12 15:34:54 +0200

    feat: implement Lexer

---
+++ src/Lexer/Lexer.cs
@@ -0,0 +1,28 @@
+﻿using Lexer.Enums;
+using Lexer.Functions;
+using Lexer.Interfaces;
+using Lexer.Types;
+
+namespace Lexer
+{
+    public class Lexer : ILexer
+    {
+        public IEnumerable<Token> Tokenize(string inputExpression)
+        {
+            int position = 0;
+            var expression = RegexFunctions.RemoveWhitespace(inputExpression);
+
+            while (position <= expression.Length)
+            {
+                var token = TokenFunctions.ToToken(expression[position..]);
+
+                yield return token;
+
+                if (token.Type is TokenType.Unknown or TokenType.EndOfFile)
+                    yield break;
+
+                position += token.Lexeme!.Length;
+            }
+        }
+    }
+}
