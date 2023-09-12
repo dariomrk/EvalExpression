19e5d4b93dfd3dd055139b77c814cb854d3cf057
dariomrk <dariomrkonjic1@gmail.com>
2023-09-12 15:33:48 +0200

    feat: add TokenTypeExtensions

---
+++ src/Lexer/Extensions/TokenTypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Lexer.Enums;
+using Lexer.Functions;
+using System.Text.RegularExpressions;
+
+namespace Lexer.Extensions
+{
+    internal static class TokenTypeExtensions
+    {
+        internal static Regex GetRegex(this TokenType type) => type switch
+        {
+            TokenType.Number => RegexFunctions.MatchNumberAtStart(),
+            TokenType.Operator => RegexFunctions.MatchOperatorAtStart(),
+            TokenType.Parenthesis => RegexFunctions.MatchParenthesisAtStart(),
+            _ => throw new ArgumentException($"Cannot provide a {nameof(Regex)} for {nameof(TokenType)}.{type}."),
+        };
+    }
+}
