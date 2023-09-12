c95ba172017f610d79e787cd0fff2b4a3bbf57d3
dariomrk <dariomrkonjic1@gmail.com>
2023-09-12 15:33:18 +0200

    feat: add RegexPattern constants

---
+++ src/Lexer/Constants/RegexPattern.cs
@@ -0,0 +1,11 @@
+﻿namespace Lexer.Constants
+{
+    internal static partial class RegexPattern
+    {
+        internal const string AtStart = "^";
+        internal const string Number = @"[0-9]+(\.[0-9]+)?";
+        internal const string Operator = "[-+*/^]";
+        internal const string Parenthesis = "[()]";
+        internal const string Whitespace = @"\s+";
+    }
+}
