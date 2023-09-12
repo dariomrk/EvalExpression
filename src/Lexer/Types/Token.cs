78d68f8adff8eba8214e69a5d157fd075a4e06a7
dariomrk <dariomrkonjic1@gmail.com>
2023-09-12 15:34:07 +0200

    feat: add Token type

---
+++ src/Lexer/Types/Token.cs
@@ -0,0 +1,33 @@
+﻿using Lexer.Enums;
+
+namespace Lexer.Types
+{
+    public readonly record struct Token
+    {
+        public string? Lexeme { get; init; }
+        public TokenType Type { get; init; }
+
+        public Token(TokenType type, string? lexeme = default)
+        {
+            Lexeme = lexeme;
+            Type = type;
+        }
+
+        public override string ToString() =>
+            $"{{ Lexeme = {Lexeme ?? "NULL"}, TokenType = {Type} }}";
+
+        public static Token EndOfFileToken =>
+            new Token
+            {
+                Lexeme = null,
+                Type = TokenType.EndOfFile
+            };
+
+        public static Token Unknown =>
+            new Token
+            {
+                Lexeme = null,
+                Type = TokenType.Unknown
+            };
+    }
+}
