452b8a3574e837234cb9bacbeb044c1028354f23
dariomrk <dariomrkonjic1@gmail.com>
2023-09-12 15:34:28 +0200

    feat: implement various Lexer functions

---
+++ src/Lexer/Functions/RegexFunctions.cs
@@ -0,0 +1,23 @@
+﻿using Lexer.Constants;
+using System.Text.RegularExpressions;
+
+namespace Lexer.Functions
+{
+    internal static partial class RegexFunctions
+    {
+        [GeneratedRegex(RegexPattern.AtStart + RegexPattern.Number)]
+        internal static partial Regex MatchNumberAtStart();
+
+        [GeneratedRegex(RegexPattern.AtStart + RegexPattern.Operator)]
+        internal static partial Regex MatchOperatorAtStart();
+
+        [GeneratedRegex(RegexPattern.AtStart + RegexPattern.Parenthesis)]
+        internal static partial Regex MatchParenthesisAtStart();
+
+        [GeneratedRegex(RegexPattern.Whitespace)]
+        internal static partial Regex MatchWhiteSpace();
+
+        internal static string RemoveWhitespace(string expression) =>
+            MatchWhiteSpace().Replace(expression, "");
+    }
+}
---
+++ src/Lexer/Functions/TokenFunctions.cs
@@ -0,0 +1,47 @@
+﻿using Lexer.Enums;
+using Lexer.Extensions;
+using Lexer.Types;
+
+namespace Lexer.Functions
+{
+    internal static class TokenFunctions
+    {
+        internal static bool TryMatchNextToken(string expression, TokenType tokenType, out Token? output)
+        {
+            output = null;
+
+            if (tokenType.GetRegex().Match(expression) is { Success: true } match)
+            {
+                output = new Token
+                {
+                    Lexeme = match.Value,
+                    Type = tokenType,
+                };
+                return true;
+            }
+
+            return false;
+        }
+
+        internal static Token ToToken(string expression)
+        {
+            if (expression.Length is 0)
+                return Token.EndOfFileToken;
+
+            var tokensToCheck = new List<TokenType>
+            {
+                TokenType.Number,
+                TokenType.Operator,
+                TokenType.Parenthesis
+            };
+
+            foreach (var tokenType in tokensToCheck)
+            {
+                if (TryMatchNextToken(expression, tokenType, out var output))
+                    return output!.Value;
+            }
+
+            return Token.Unknown;
+        }
+    }
+}
