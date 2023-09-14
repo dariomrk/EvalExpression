602427afcdf9a0fe6391077130308cfbb696810b
dariomrk <dariomrkonjic1@gmail.com>
2023-09-14 18:47:19 +0200

    feat: add TokenExtensions

---
+++ src/Parser/Extensions/TokenExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Lexer.Enums;
+using Lexer.Types;
+using Parser.Enums;
+
+namespace Parser.Extensions
+{
+    internal static class TokenExtensions
+    {
+        internal static Operator GetOperator(this Token token)
+        {
+            if (token.Type is TokenType.EndOfFile)
+                return Operator.None;
+
+            if (token.Type is not TokenType.Operator)
+                throw new ArgumentException($"Cannot provide an {nameof(Operator)} for {nameof(Token)}.Type of {token.Type}.");
+
+            return token.Lexeme switch
+            {
+                "+" => Operator.Add,
+                "-" => Operator.Subtract,
+                "*" => Operator.Multiply,
+                "/" => Operator.Divide,
+                "^" => Operator.Exponentiate,
+                "(" => Operator.OpenParenthesis,
+                ")" => Operator.CloseParenthesis,
+                _ => throw new InvalidOperationException(),
+            };
+        }
+    }
+}
