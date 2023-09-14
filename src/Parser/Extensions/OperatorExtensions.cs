7bfa433f410438c9991cde8d8ee5ed84971a9a92
dariomrk <dariomrkonjic1@gmail.com>
2023-09-14 18:47:09 +0200

    feat: add OperatorExtensions

---
+++ src/Parser/Extensions/OperatorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Parser.Enums;
+using System.Collections.Frozen;
+
+namespace Parser.Extensions
+{
+    internal static class OperatorExtensions
+    {
+        private static readonly FrozenDictionary<Operator, int> _precedenceTable = new Dictionary<Operator, int>
+        {
+            { Operator.None, 0 },
+            { Operator.Add, 1 },
+            { Operator.Subtract, 1 },
+            { Operator.Multiply, 2 },
+            { Operator.Divide, 2 },
+            { Operator.Exponentiate, 3 },
+            { Operator.OpenParenthesis, 4 },
+            { Operator.CloseParenthesis, 4 },
+        }.ToFrozenDictionary();
+
+        internal static int GetPrecedence(this Operator @operator)
+        {
+            return _precedenceTable[@operator];
+        }
+    }
+}
