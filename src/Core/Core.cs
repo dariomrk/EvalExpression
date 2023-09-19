67ebb3fedd4221e09cef5aa6b36033cdc9486481
dariomrk <dariomrkonjic1@gmail.com>
2023-09-19 17:49:06 +0200

    feat: implement Core Evaluate

--- src/Core/Core.cs
+++ src/Core/Core.cs
@@ -1,6 +1,24 @@
-﻿namespace Core
+﻿using Lexer.Enums;
+
+namespace Core
 {
-    public class Core
+    public static class Core
     {
+        public static decimal Evaluate(
+            string expression,
+            bool ignoreWhitespace = true)
+        {
+            var tokens = Lexer.Lexer
+                .Tokenize(expression)
+                .Where(token => token.Type is not TokenType.Whitespace && ignoreWhitespace);
+
+            var parser = new Parser.Parser(tokens);
+
+            var abstractSyntaxTree = parser.Parse();
+
+            var result = Interpreter.Interpreter.Evaluate(abstractSyntaxTree);
+
+            return result;
+        }
     }
 }
