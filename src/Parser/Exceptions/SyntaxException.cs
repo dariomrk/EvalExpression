9184ca6a9680ebb118d54615a1824a350b9a71e4
dariomrk <dariomrkonjic1@gmail.com>
2023-09-19 17:15:51 +0200

    chore: remove unused SyntaxException ctor

--- src/Parser/Exceptions/SyntaxException.cs
+++ src/Parser/Exceptions/SyntaxException.cs
@@ -2,7 +2,6 @@
 {
     public sealed class SyntaxException : Exception
     {
-        internal SyntaxException() { }
         internal SyntaxException(string? message) : base(message) { }
     }
 }
