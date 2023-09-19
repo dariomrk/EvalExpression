ef43704d5589d031968b96fe59dceae97bf9040b
dariomrk <dariomrkonjic1@gmail.com>
2023-09-19 15:55:08 +0200

    feat: add SyntaxException

---
+++ src/Parser/Exceptions/SyntaxException.cs
@@ -0,0 +1,8 @@
+﻿namespace Parser.Exceptions
+{
+    public sealed class SyntaxException : Exception
+    {
+        internal SyntaxException() { }
+        internal SyntaxException(string? message) : base(message) { }
+    }
+}
