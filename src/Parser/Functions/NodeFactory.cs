926c15a0193c279cc200824504d5dca0026bbd9d
dariomrk <dariomrkonjic1@gmail.com>
2023-09-19 15:55:37 +0200

    feat: add Parser functions

---
+++ src/Parser/Functions/NodeFactory.cs
@@ -0,0 +1,17 @@
+﻿using Parser.Enums;
+using Parser.Types;
+
+namespace Parser.Functions
+{
+    internal static class NodeFactory
+    {
+        internal static Node NumericLiteral(string value) =>
+            new NumericLiteralNode(decimal.Parse(value));
+
+        internal static Node Binary(NodeType nodeType, Node left, Node right) =>
+            new BinaryNode(nodeType, left, right);
+
+        internal static Node Unary(NodeType nodeType, Node next) =>
+            new UnaryNode(nodeType, next);
+    }
+}
---
+++ src/Parser/Functions/ThrowHelper.cs
@@ -0,0 +1,21 @@
+﻿using Lexer.Enums;
+using Parser.Exceptions;
+
+namespace Parser.Functions
+{
+    internal static class ThrowHelper
+    {
+        internal static void UnexpectedEndOfExpression(TokenType expected) =>
+            throw new SyntaxException($"Unexpected end of expression, expected {nameof(TokenType)} of {expected}");
+
+        internal static void UnexpectedEndOfExpression(HashSet<TokenType> expectedCollection)
+        {
+            var expected = expectedCollection.Aggregate("", (token, accumulator) => $"{accumulator}, {token}");
+
+            throw new SyntaxException($"Unexpected end of expression, expected {nameof(TokenType)} of: [ {expected}]");
+        }
+
+        internal static void UnexpectedToken(string? lexeme, TokenType expected) =>
+            throw new SyntaxException($"Unexpected token: '{lexeme ?? "NULL"}', expected {nameof(TokenType)} of {expected}");
+    }
+}
