87e0a171e4dc668c11050a5e08ec8f3309fa7d66
dariomrk <dariomrkonjic1@gmail.com>
2023-10-05 15:14:26 +0200

    feat: handle expected end of file

--- src/Parser/Functions/ThrowHelper.cs
+++ src/Parser/Functions/ThrowHelper.cs
@@ -17,5 +17,8 @@ namespace Parser.Functions
 
         internal static void UnexpectedToken(string? lexeme, TokenType expected) =>
             throw new SyntaxException($"Unexpected token: '{lexeme ?? "NULL"}', expected {nameof(TokenType)} of {expected}.");
+
+        internal static void ExpectedEndOfFile(string? lexeme) =>
+            throw new SyntaxException($"Unexpected token: '{lexeme ?? "NULL"}', expected {nameof(TokenType)}.{nameof(TokenType.EndOfFile)}.");
     }
 }
